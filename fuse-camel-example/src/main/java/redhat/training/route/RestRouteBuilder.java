b6dede0a56ff4ef28329c3138a63ad29f8d22aec
Frankie Haro <fharo@fharo.remote.csb>
2020-04-30 09:06:13 -0700

    Adding code for using predicates, setting headers, and content based routing.

--- fuse-camel-example/src/main/java/redhat/training/route/RestRouteBuilder.java
+++ fuse-camel-example/src/main/java/redhat/training/route/RestRouteBuilder.java
@@ -1,7 +1,11 @@
 package redhat.training.route;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.XPathBuilder;
 import org.springframework.stereotype.Component;
+import org.w3c.dom.NodeList;
 
 @Component
 public class RestRouteBuilder extends RouteBuilder{
@@ -25,6 +29,36 @@ public class RestRouteBuilder extends RouteBuilder{
 		    + "  greeting: Hello, ${header.name}\n"
 		    + "  server: " + System.getenv("HOSTNAME") + "\n"
 		    + "}\n");
+		
+		
+		// CBR Route builder
+		final var GET_VENDORNAME_HEADER = header("vendorName");
+		final var GET_SKIPORDER_HEADER = header("skipOrder");
+		
+		final var GET_ORDERITEMPUBLISHERNAME_XPATH = xpath("/order/orderItems/orderItem/orderItemPublisherName/text()");
+		final var GET_ORDERID_XPATH = xpath("/order/orderId/text()");
+		
+		from("file:orders/incoming?include=order.*xml")
+		.process((Exchange exchange)->{
+			NodeList test = XPathBuilder.xpath("/order/test").evaluate(exchange, NodeList.class);
+			if(test.getLength() > 0) {
+				log.info("Adding skipOrder header");
+				exchange.getIn().setHeader("skipOrder", "Y");
+			}
+		})
+		.filter(GET_SKIPORDER_HEADER.isNull())
+		.setHeader("orderId", GET_ORDERID_XPATH)
+		.setHeader("vendorName", GET_ORDERITEMPUBLISHERNAME_XPATH)
+		.choice()
+			.when(GET_VENDORNAME_HEADER.isEqualTo("ABC Company"))
+				.log("sending order ${header.orderId} to folder abc.")
+				.to("file:orders/outgoing/abc")
+			.when(GET_VENDORNAME_HEADER.isEqualTo("ORly"))
+				.log("sending order ${header.orderId} to folder orly.")
+				.to("file:orders/outgoing/orly")
+			.when(GET_VENDORNAME_HEADER.isEqualTo("Namming"))
+				.log("sending order ${header.orderId} to folder namming.")
+				.to("file:orders/outgoing/namming");
 	}
 
 
